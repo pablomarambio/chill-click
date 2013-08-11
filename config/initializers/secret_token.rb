4e4a3e1c06b0e49801f21e317c18c1cdcc7f33e7
pablomarambio <pablo@pablomarambio.cl>
2013-08-11 11:40:26 -0400

    App en blanco

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChillClick::Application.config.secret_token = '4a386a73ad74dce4a864dcb9fdf1c5be88f6a99dd2bd8697ba851bc22a6a926a96f3fc9acd0af386120dedaf6d19b867c8247edbf75ade64931bb8ed640928cc'
