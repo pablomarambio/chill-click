5adb4ae1ad8fb0596f7ecd52ea585a92ebb23bd4
pablomarambio <pablo@pablomarambio.cl>
2013-08-11 12:48:04 -0400

    CRUD Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
-  
-  def index
-    @users = User.all
-  end
-  
+	
+	def index
+		@users = User.all
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new
+		@user.update_attributes(params[:user])
+		@user.save!
+		redirect_to @user
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update_attributes(params[:user])
+		@user.save!
+		redirect_to @user
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+	
 end
